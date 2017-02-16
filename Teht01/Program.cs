7821c05176fa702ee19b612d03b1875ce165110e
niko <niko@KONE>
2017-02-16 12:13:15 +0200

    asd

--- Teht01/Program.cs
+++ Teht01/Program.cs
@@ -10,6 +10,24 @@ namespace Teht01
     {
         static void Main(string[] args)
         {
+
+            // Tee ohjelma, joka kirjoittaa käyttäjän antamat merkkijonot tiedostoon (lopetusehdon voit päättää itse) ja sulje tiedosto. 
+            // Avaa tämän jälkeen tiedosto lukemista varten ja tulosta näyttölaitteelle tiedoston sisältö riveittäin.
+
+            string sana = "pena";
+
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter(@"d:\test.txt");
+            while (sana!=" ")
+            {
+                Console.WriteLine("Give a text line (spacebar quits) ");
+                sana = Console.ReadLine();
+                outputFile.WriteLine(sana);
+            }
+            outputFile.Close();
+
+            string text = System.IO.File.ReadAllText(@"d:\test.txt");
+            System.Console.WriteLine("Contents of test.txt:\n"+ text);
+
         }
     }
 }
